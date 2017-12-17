aaf18d522a1c931e751b91e49978a7a47d3f1da8
svanhesteren <s.vanhesteren@gmail.com>
2017-12-17 16:44:53 +0100

    Pass G, all tests pass now.

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -20,37 +20,35 @@ class Codebreaker
       number == 4
     end
 
-
-
     def get_match_char(number, position)
-      match_char = ""
-      return "" if @used_numbers.include?(number)
+        match_char = ""
+        return "" if @used_numbers.include?(number)
 
-      if @secret_number.include?(number)
-        @used_numbers << number
-        if exact_match?(number, position)
-          match_char = "+"
-        else
-          match_char = "-"
+        if @secret_number.include?(number)
+          @used_numbers << number
+          if exact_match?(number, position)
+            match_char = "+"
+          else
+            match_char = "-"
+          end
         end
+        match_char
       end
-      match_char
-    end
-
-    # def check_last_num_override(number)
-    #   if @used_numbers.include?(number) &&
-    # end
 
     def guess(input)
       result_string = ""
       @used_numbers = ""
       return output.puts "Try guessing a number with four digits" if !right_size?(input.length)
-      for num_pos in (0...input.length)
 
+      for num_pos in (0...input.length)
         result_string << get_match_char(input[num_pos], num_pos)
-        # result_string = check_last_num_override(number) if num_pos == 3
       end
-        output.puts result_string.chars.sort.join
+
+      if @used_numbers.count(input[3]) && exact_match?(input[3], 3)
+        result_string.sub!("-", "+")
+      end
+
+      output.puts result_string.chars.sort.join
     end
 
   end
