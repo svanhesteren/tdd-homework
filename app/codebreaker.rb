c4d5762cd77d9b9958cbca6ca42a077307913792
svanhesteren <s.vanhesteren@gmail.com>
2017-12-17 13:30:30 +0100

    Pass B and C tests

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -15,11 +15,41 @@ class Codebreaker
       # output.puts "Try guessing a number with four digits"
     end
 
-    def guess(input)
+    def num_equal?(number, position)
+      number == @secret_number[position]
+    end
+    def num_match(number, position)
+      num_equal?(number, position) ? "+" : ""
+    end
 
-      if input.length != 4
-        output.puts "Try guessing a number with four digits"
-      end
+    def right_size?(number)
+      number == 4
+    end
+
+    def guess(input)
+      result_string = ""
+
+      # if input.length != 4
+      return output.puts "Try guessing a number with four digits" if !right_size?(input.length)
+      # else
+      # puts input
+        for num in (0...input.length)
+          match_symbol = num_match(input[num], num)
+          if match_symbol == ""
+            if @secret_number.include?(input[num])
+              result_string << "-"
+            else
+              result_string << match_symbol
+            end
+          else
+            result_string << match_symbol
+          end
+        end
+        output.puts result_string
+      # end
+
+
+
       # Make sure to replace next line with the actual implemented marking algorithm,
       # using the @secret_number
 
